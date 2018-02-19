606f80d1d3c960d5e27c52a914aad81673f6aa1b
Roberto Srebot <rsrebot@digitalriver.com>
2018-02-19 12:15:40 -0300

    working on security component

--- src/app/features/report-details/security/security.component.ts
+++ src/app/features/report-details/security/security.component.ts
@@ -13,7 +13,13 @@ export class SecurityComponent implements OnInit {
     this.reportDefinition = val;
   }
 
-  groups: ['A', 'Ab', 'Abc'];
+  groups = ['A', 'Ab', 'Abc', 'aaaaaaaaaaaaaa asdasd asdasasdas', 'aaaaaaaaaaaaaa asdasd asdasasdas', 'aaaaaaaaaaaaaa asdasd asdasasdas',
+  'aaaaaaaaaaaaaa asdasd asdasasdas', 'aaaaaaaaaaaaaa asdasd asdasasdas', 'aaaaaaaaaaaaaa asdasd asdasasdas',
+  'aaaaaaaaaaaaaa asdasd asdasasdas', 'aaaaaaaaaaaaaa asdasd asdasasdas', 'aaaaaaaaaaaaaa asdasd asdasasdas',
+  'aaaaaaaaaaaaaa asdasd asdasasdas', 'aaaaaaaaaaaaaa asdasd asdasasdas', 'aaaaaaaaaaaaaa asdasd asdasasdas'];
+
+  assignedGroups: string[] = [];
+
   selected: string;
 
   reportDefinition: QueryDefViewModel;
@@ -21,11 +27,57 @@ export class SecurityComponent implements OnInit {
   constructor() { }
 
   assignGroup(group: string) {
+    const i = this.groups.findIndex((value, index, obj) => {
+      return value === group;
+    });
+    const val = this.groups.splice(i,  i + 1);
+    if (val.length > 0) {
+      this.assignedGroups.push(val[0]);
+    }
 
+    this.sortGroups();
+  }
+
+  sortGroups() {
+    this.assignedGroups.sort((a, b) => {
+       if (a === b) {
+          return 0;
+        }
+
+        if (a < b) {
+          return 1;
+        }
+
+        if (a > b) {
+          return -1;
+        }
+    });
+
+    this.groups.sort((a, b) => {
+       if (a === b) {
+          return 0;
+        }
+
+        if (a < b) {
+          return 1;
+        }
+
+        if (a > b) {
+          return -1;
+        }
+    });
   }
 
   removeGroup(group: string) {
+    const i = this.assignedGroups.findIndex((value, index, obj) => {
+      return value === group;
+    });
+    const val = this.assignedGroups.splice(i, i +  1);
+    if (val.length > 0) {
+      this.groups.push(val[0]);
+    }
 
+    this.sortGroups();
   }
 
   ngOnInit() {
